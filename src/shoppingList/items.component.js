3d09ee13af385361010149f34b12e82035f46528
robbieskillen <rskill01@baker.edu>
2016-11-27 12:43:27 -0500

    test

--- src/shoppingList/items.component.js
+++ src/shoppingList/items.component.js
@@ -2,7 +2,7 @@
 	'use strict';
 	angular.module('MenuApp')
 	.component('items', {
-		templateUrl: 'src/shoppinglist/templates/items.template.html',
+		templateUrl: 'src/shoppingList/templates/items.template.html',
 		bindings: {
 		   items: '<'
 		}
