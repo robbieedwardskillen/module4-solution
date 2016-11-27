58b4515e18e06dc85a493d6a7a16be8f43d39256
robbieskillen <rskill01@baker.edu>
2016-11-26 21:49:46 -0500

    test

--- src/shoppingList/items.controller.js
+++ src/shoppingList/items.controller.js
@@ -4,9 +4,9 @@
 angular.module('MenuApp')
 .controller('ItemsController', ItemsController);
 
-ItemsController.$inject = ['items'];
-function ItemsController(items) {
-// function ItemsController() {
+// ItemsController.$inject = ['items'];
+// function ItemsController(items) {
+function ItemsController() {
 	  var itemList = this;
   	  itemList.items = items;
 }
